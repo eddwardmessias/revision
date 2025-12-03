da1af41546eb0ad41febc34d98dd2f5142d902f2
eddwardmessias <eddwardmessias@hotmail.com>
2025-12-03 12:44:01 -0300

    feat: implement data persistence method get

--- server.ts
+++ server.ts
@@ -1,5 +1,8 @@
 import fastify from 'fastify'
 import crypto from 'node:crypto'
+import { eq } from 'drizzle-orm'
+import { db } from './src/database/client.ts'
+import { courses } from './src/database/schema.ts'
 
 const server = fastify({
   logger: {
@@ -13,18 +16,24 @@ const server = fastify({
   }
 })
 
-const courses = [
-  { id: '1', title: 'NodeJS' },
-  { id: '2', title: 'ReactJS' },
-  { id: '3', title: 'React Native' },
-]
+// const courses = [
+//   { id: '1', title: 'NodeJS' },
+//   { id: '2', title: 'ReactJS' },
+//   { id: '3', title: 'React Native' },
+// ]
 
-server.get('/courses', (request, reply) => {
-  return reply.send({ courses, page: 1 })
+server.get('/courses', async (request, reply) => {
+
+  const result = await db.select({
+    id: courses.id,
+    title: courses.title
+  }).from(courses)
+
+  return reply.send({ courses: result})
 })
 
 
-server.get('/courses/:id', (request, reply) => {
+server.get('/courses/:id', async (request, reply) => {
   
   type Params = {
     id: string
@@ -32,36 +41,39 @@ server.get('/courses/:id', (request, reply) => {
 
   const params = request.params as Params
 
-  const { id } = params 
+  const courseId = params.id
 
-  const course = courses.find(course => course.id === id)
+  const result = await db
+  .select()
+  .from(courses)
+  .where(eq(courses.id, courseId))
 
-  if (!course) {
-    return reply.status(404).send({ error: 'Course not found' })
+  if (result.length > 0) {
+    return { course: result[0] }
   }
 
-  return { course }
+  return reply.status(404).send({ error: 'Course not found' })
 })
 
-server.post('/courses', (request, reply) => {
+// server.post('/courses', (request, reply) => {
 
-  type Body = {
-    title: string
-  }
+//   type Body = {
+//     title: string
+//   }
 
-  const body = request.body as Body
+//   const body = request.body as Body
 
-  const coursesId = crypto.randomUUID()
+//   const coursesId = crypto.randomUUID()
 
-  const courseTitle = body.title
+//   const courseTitle = body.title
 
-  if (!courseTitle) {
-    return reply.status(400).send({ error: 'Title is required' })
-  }
+//   if (!courseTitle) {
+//     return reply.status(400).send({ error: 'Title is required' })
+//   }
 
-  courses.push({ id: coursesId, title: courseTitle })
-  return reply.status(201).send({ coursesId })
-})
+//   courses.push({ id: coursesId, title: courseTitle })
+//   return reply.status(201).send({ coursesId })
+// })
 
 server.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server running on http://localhost:3333')
