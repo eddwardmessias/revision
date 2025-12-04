6aa46659061723826168c8e06fb7ca8eecc9cce1
eddwardmessias <eddward.messias@hotmail.com>
2025-12-04 10:19:13 -0300

    refactor:  pagination get-courses

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -2,7 +2,7 @@ import type {FastifyPluginAsyncZod} from 'fastify-type-provider-zod'
 import { db } from '../database/client.ts'
 import { courses } from '../database/schema.ts'
 import z from 'zod'
-import { ilike, asc } from 'drizzle-orm'
+import { ilike, asc, and, type SQL } from 'drizzle-orm'
 
 export const  getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
   server.get('/courses', {
@@ -29,7 +29,14 @@ export const  getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
   }, async (request, reply) => {
     const { search, orderBy, page} = request.query
 
-    const result = await db.select({
+    const conditions: SQL[] = []
+
+    if(search){
+      conditions.push(ilike(courses.title, `%${search}%`))
+    }
+
+    const [result, total] = await Promise.all([
+      db.select({
         id: courses.id,
         title: courses.title
       })
@@ -37,11 +44,9 @@ export const  getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
       .orderBy(asc(courses[orderBy]))
       .offset((page - 1) * 2)
       .limit(2)
-      .where(
-        search ? ilike(courses.title, `%${search}%`) : undefined
-      )
-
-    const total = await db.$count(courses,search ? ilike(courses.title, `%${search}%`) : undefined)
+      .where(and(...conditions)),
+      db.$count(courses, ...conditions)
+    ])
 
     return reply.send({ courses: result, total})
   })
